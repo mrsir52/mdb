55c410b2a834e961b2b7e6d5c3b8bcfe011a123e
Manny <coachmannytw@gmail.com>
2019-11-14 20:52:19 -0700

    separated out Event into it's own component

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ import {
   MDBCol
 } from "mdbreact";
 import "./index.css";
+import Event from "./Event"
+
+
 class App extends Component {
   state = {
     modal: false,
@@ -202,44 +205,5 @@ class App extends Component {
   }
 }
 
-class Event extends Component {
-  state = {
-    time: "11:00",
-    title: "My state title"
-  };
-  render() {
-    return (
-      <React.Fragment>
-        <div className="media mt-1">
-          <h3 className="h3-responsive font-weight-bold mr-3">
-            {this.props.time}
-          </h3>
-          <div className="media-body mb-3 mb-lg-3">
-            <MDBBadge
-              color="danger"
-              className="ml-2 float-right"
-              onClick={() => this.props.onDelete(this.props.id)}
-            >
-              -
-            </MDBBadge>
-            <h6 className="mt-0 font-weight-bold">{this.props.title} </h6>{" "}
-            <hr className="hr-bold my-2" />
-            {this.props.location && (
-              <React.Fragment>
-                <p className="font-smaller mb-0">
-                  <MDBIcon icon="location-arrow" /> {this.props.location}
-                </p>
-              </React.Fragment>
-            )}
-          </div>
-        </div>
-        {this.props.description && (
-          <p className="p-2 mb-4  blue-grey lighten-5 blue-grey lighten-5">
-            {this.props.description}
-          </p>
-        )}
-      </React.Fragment>
-    );
-  }
-}
+
 ReactDOM.render(<App />, document.getElementById("root"));
